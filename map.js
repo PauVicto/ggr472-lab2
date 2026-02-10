abf9c784514d326523933eaa36286620b3efc95d
PauVicto <victoria.pau@mail.utoronto.ca>
2026-02-09 23:58:14 -0500

    cafe point json edits

--- map.js
+++ map.js
@@ -64,8 +64,19 @@ map.on('load', () => {
             'fill-opacity': 0.3
         }
     });
-    map.addSource('cafes_along_route', {
-
+    map.addSource('cafes_along_route', { 
+        'type': 'geojson',
+        'data': 'https://raw.githubusercontent.com/PauVicto/ggr472-lab2/refs/heads/main/data/cafes_along_route.geojson'     
+    });
+    map.addLayer({
+        'id': 'cafes_along_route',
+        'type': 'symbol',
+        'source': 'cafes_along_route',
+        'layout': {
+            'icon-image': 'cafe',
+            'icon-size': 1.5,
+            'icon-allow-overlap': true
+        }
     });    
 });
 
